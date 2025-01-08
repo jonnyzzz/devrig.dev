f980638eb98c14f932448ec942b09b2aa91f2599
Eugene Petrenko <me@jonnyzzz.com>
2025-01-08 16:59:33 +0100

    Add main command execution and config file handling
    Introduced a `runMainCommand` function to execute the root command logic. This includes locating a configuration file using the added `config` package and logging errors if the file is missing. Enhanced the CLI tool's usability by integrating these improvements.

--- cli/main.go
+++ cli/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"cli/config"
 	"fmt"
 	"github.com/spf13/cobra"
+	"log"
 	"os"
 )
 
 var rootCmd = &cobra.Command{
 	Use:   "idew",
 	Short: fmt.Sprintf("IDE Wrapper v%s is your development entry point", VersionAndBuild()),
+	Run:   runMainCommand,
 }
 
 func init() {
@@ -19,4 +22,14 @@ func main() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
+}
+
+func runMainCommand(cmd *cobra.Command, args []string) {
+	configPath, err := config.FindConfigFile(".")
+	if err != nil {
+		log.Fatalln("Failed to find config file. ", err)
+	}
+
+	fmt.Println("Config file:", configPath)
+
 }
